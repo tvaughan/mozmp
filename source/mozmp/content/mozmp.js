c714b7c3c15bc002ede69851331c78dc893f9c20
tom <tom>
2001-02-13 21:24:16 +0000

    Clutter.

--- source/mozmp/content/mozmp.js
+++ source/mozmp/content/mozmp.js
@@ -17,21 +17,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-function mozmpOnLoad()
-{
-	return;
-}
-
-function mozmpOnUnload()
-{
-	return;
-}
-
-function mozmpOnClose()
-{
-	return;
-}
-
 function mozmpExit()
 {
 	close();
